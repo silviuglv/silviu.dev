db7b0431eedd33182f8570fd88c00fd79773803c
Silviu Glavan <silviu@gigs.ro>
2025-10-10 23:28:31 +0300

    chore: update deps

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -18,7 +18,7 @@ export default async function FAQ() {
   return (
     <section className="py-16">
       <div className="container">
-        <div className="prose dark:prose-invert prose-sm max-w-none">
+        <div className="prose dark:prose-invert max-w-none">
           <h1>FAQ</h1>
           <p>Find out more about me, my opinions, and personal preferences.</p>
           <FAQList data={faq} />
